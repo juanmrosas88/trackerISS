36ff8ba5ff0f0db849bf4cff10b2e8acd22f1a24
Juan M Rosas <juanmrosas8@gmail.com>
2018-09-11 18:37:51 -0300

    agregue un txt de prueba

---
+++ BuscadordeSTRINGenWeb/Form1.Designer.vb
@@ -0,0 +1,244 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim ChartArea1 As System.Windows.Forms.DataVisualization.Charting.ChartArea = New System.Windows.Forms.DataVisualization.Charting.ChartArea()
+        Dim Legend1 As System.Windows.Forms.DataVisualization.Charting.Legend = New System.Windows.Forms.DataVisualization.Charting.Legend()
+        Dim Series1 As System.Windows.Forms.DataVisualization.Charting.Series = New System.Windows.Forms.DataVisualization.Charting.Series()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.txtCodigo = New System.Windows.Forms.TextBox()
+        Me.Chart1 = New System.Windows.Forms.DataVisualization.Charting.Chart()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.TabControl1 = New System.Windows.Forms.TabControl()
+        Me.TabPage1 = New System.Windows.Forms.TabPage()
+        Me.TabPage2 = New System.Windows.Forms.TabPage()
+        Me.GMapControl1 = New GMap.NET.WindowsForms.GMapControl()
+        Me.TabPage3 = New System.Windows.Forms.TabPage()
+        Me.WebBrowser1 = New System.Windows.Forms.WebBrowser()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.LblURL = New System.Windows.Forms.Label()
+        Me.LblURLWiki = New System.Windows.Forms.Label()
+        CType(Me.Chart1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.TabControl1.SuspendLayout()
+        Me.TabPage1.SuspendLayout()
+        Me.TabPage2.SuspendLayout()
+        Me.TabPage3.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'txtCodigo
+        '
+        Me.txtCodigo.BackColor = System.Drawing.SystemColors.MenuText
+        Me.txtCodigo.ForeColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.txtCodigo.Location = New System.Drawing.Point(12, 43)
+        Me.txtCodigo.Multiline = True
+        Me.txtCodigo.Name = "txtCodigo"
+        Me.txtCodigo.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.txtCodigo.Size = New System.Drawing.Size(208, 82)
+        Me.txtCodigo.TabIndex = 3
+        '
+        'Chart1
+        '
+        Me.Chart1.BackColor = System.Drawing.Color.CornflowerBlue
+        Me.Chart1.BackGradientStyle = System.Windows.Forms.DataVisualization.Charting.GradientStyle.TopBottom
+        ChartArea1.Name = "ChartArea1"
+        Me.Chart1.ChartAreas.Add(ChartArea1)
+        Legend1.Name = "Legend1"
+        Me.Chart1.Legends.Add(Legend1)
+        Me.Chart1.Location = New System.Drawing.Point(6, 6)
+        Me.Chart1.Name = "Chart1"
+        Series1.ChartArea = "ChartArea1"
+        Series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline
+        Series1.Legend = "Legend1"
+        Series1.Name = "Series1"
+        Me.Chart1.Series.Add(Series1)
+        Me.Chart1.Size = New System.Drawing.Size(973, 687)
+        Me.Chart1.TabIndex = 5
+        Me.Chart1.Text = "Chart1"
+        '
+        'ListBox1
+        '
+        Me.ListBox1.BackColor = System.Drawing.SystemColors.MenuText
+        Me.ListBox1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 16
+        Me.ListBox1.Location = New System.Drawing.Point(12, 131)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(208, 580)
+        Me.ListBox1.TabIndex = 2
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        Me.Timer1.Interval = 10000
+        '
+        'TabControl1
+        '
+        Me.TabControl1.Controls.Add(Me.TabPage1)
+        Me.TabControl1.Controls.Add(Me.TabPage2)
+        Me.TabControl1.Controls.Add(Me.TabPage3)
+        Me.TabControl1.Location = New System.Drawing.Point(236, 43)
+        Me.TabControl1.Name = "TabControl1"
+        Me.TabControl1.SelectedIndex = 0
+        Me.TabControl1.Size = New System.Drawing.Size(1175, 697)
+        Me.TabControl1.TabIndex = 6
+        '
+        'TabPage1
+        '
+        Me.TabPage1.BackColor = System.Drawing.Color.Transparent
+        Me.TabPage1.Controls.Add(Me.Chart1)
+        Me.TabPage1.ImeMode = System.Windows.Forms.ImeMode.NoControl
+        Me.TabPage1.Location = New System.Drawing.Point(4, 25)
+        Me.TabPage1.Name = "TabPage1"
+        Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage1.Size = New System.Drawing.Size(1167, 668)
+        Me.TabPage1.TabIndex = 0
+        Me.TabPage1.Text = "Grafico"
+        Me.TabPage1.ToolTipText = "Muestra los datos en un grafico"
+        '
+        'TabPage2
+        '
+        Me.TabPage2.Controls.Add(Me.GMapControl1)
+        Me.TabPage2.Location = New System.Drawing.Point(4, 25)
+        Me.TabPage2.Name = "TabPage2"
+        Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage2.Size = New System.Drawing.Size(1167, 668)
+        Me.TabPage2.TabIndex = 1
+        Me.TabPage2.Text = "Mapa"
+        Me.TabPage2.UseVisualStyleBackColor = True
+        '
+        'GMapControl1
+        '
+        Me.GMapControl1.Bearing = 0!
+        Me.GMapControl1.CanDragMap = True
+        Me.GMapControl1.EmptyTileColor = System.Drawing.Color.Navy
+        Me.GMapControl1.GrayScaleMode = False
+        Me.GMapControl1.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow
+        Me.GMapControl1.LevelsKeepInMemmory = 5
+        Me.GMapControl1.Location = New System.Drawing.Point(7, 7)
+        Me.GMapControl1.MarkersEnabled = True
+        Me.GMapControl1.MaxZoom = 2
+        Me.GMapControl1.MinZoom = 2
+        Me.GMapControl1.MouseWheelZoomEnabled = True
+        Me.GMapControl1.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter
+        Me.GMapControl1.Name = "GMapControl1"
+        Me.GMapControl1.NegativeMode = False
+        Me.GMapControl1.PolygonsEnabled = True
+        Me.GMapControl1.RetryLoadTile = 0
+        Me.GMapControl1.RoutesEnabled = True
+        Me.GMapControl1.ScaleMode = GMap.NET.WindowsForms.ScaleModes.[Integer]
+        Me.GMapControl1.SelectedAreaFillColor = System.Drawing.Color.FromArgb(CType(CType(33, Byte), Integer), CType(CType(65, Byte), Integer), CType(CType(105, Byte), Integer), CType(CType(225, Byte), Integer))
+        Me.GMapControl1.ShowTileGridLines = False
+        Me.GMapControl1.Size = New System.Drawing.Size(1154, 675)
+        Me.GMapControl1.TabIndex = 0
+        Me.GMapControl1.Zoom = 0R
+        '
+        'TabPage3
+        '
+        Me.TabPage3.Controls.Add(Me.WebBrowser1)
+        Me.TabPage3.Location = New System.Drawing.Point(4, 25)
+        Me.TabPage3.Name = "TabPage3"
+        Me.TabPage3.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage3.Size = New System.Drawing.Size(1167, 668)
+        Me.TabPage3.TabIndex = 2
+        Me.TabPage3.Text = "Wikipedia"
+        Me.TabPage3.UseVisualStyleBackColor = True
+        '
+        'WebBrowser1
+        '
+        Me.WebBrowser1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.WebBrowser1.IsWebBrowserContextMenuEnabled = False
+        Me.WebBrowser1.Location = New System.Drawing.Point(3, 3)
+        Me.WebBrowser1.MinimumSize = New System.Drawing.Size(20, 20)
+        Me.WebBrowser1.Name = "WebBrowser1"
+        Me.WebBrowser1.Size = New System.Drawing.Size(1161, 662)
+        Me.WebBrowser1.TabIndex = 0
+        Me.WebBrowser1.Url = New System.Uri("https://es.wikipedia.org/wiki/Estaci%C3%B3n_Espacial_Internacional", System.UriKind.Absolute)
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(9, 723)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(147, 17)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "Cantidad de puntos: 1"
+        '
+        'LblURL
+        '
+        Me.LblURL.AutoSize = True
+        Me.LblURL.Location = New System.Drawing.Point(12, 12)
+        Me.LblURL.Name = "LblURL"
+        Me.LblURL.Size = New System.Drawing.Size(55, 17)
+        Me.LblURL.TabIndex = 8
+        Me.LblURL.Text = "LblURL"
+        '
+        'LblURLWiki
+        '
+        Me.LblURLWiki.AutoSize = True
+        Me.LblURLWiki.Location = New System.Drawing.Point(415, 12)
+        Me.LblURLWiki.Name = "LblURLWiki"
+        Me.LblURLWiki.Size = New System.Drawing.Size(81, 17)
+        Me.LblURLWiki.TabIndex = 11
+        Me.LblURLWiki.Text = "LblURLWiki"
+        Me.LblURLWiki.Visible = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(1423, 752)
+        Me.Controls.Add(Me.LblURLWiki)
+        Me.Controls.Add(Me.LblURL)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TabControl1)
+        Me.Controls.Add(Me.txtCodigo)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "Estacion Espacial Internacional"
+        CType(Me.Chart1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.TabControl1.ResumeLayout(False)
+        Me.TabPage1.ResumeLayout(False)
+        Me.TabPage2.ResumeLayout(False)
+        Me.TabPage3.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents txtCodigo As TextBox
+    Friend WithEvents Chart1 As DataVisualization.Charting.Chart
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents TabControl1 As TabControl
+    Friend WithEvents TabPage1 As TabPage
+    Friend WithEvents TabPage2 As TabPage
+    Friend WithEvents GMapControl1 As GMap.NET.WindowsForms.GMapControl
+    Friend WithEvents Label2 As Label
+    Friend WithEvents LblURL As Label
+    Friend WithEvents LblURLWiki As Label
+    Friend WithEvents TabPage3 As TabPage
+    Friend WithEvents WebBrowser1 As WebBrowser
+End Class
---
+++ BuscadordeSTRINGenWeb/Form1.vb
@@ -0,0 +1,274 @@
+﻿Imports System.IO
+Imports System.Net
+Imports System.Text
+Imports System.Windows.Forms.DataVisualization.Charting
+Imports System.Windows.Forms
+
+Imports GMap.NET
+Imports GMap.NET.MapProviders
+Imports GMap.NET.WindowsForms
+Imports GMap.NET.WindowsForms.Markers
+
+Imports Newtonsoft.Json
+Imports System.ComponentModel
+
+Public Class Form1
+#Region "Variables"
+
+    Public Class Rootobject
+
+        Public Property Message As String
+        Public Property Iss_position As Iss_Position
+        Public Property Timestamp As Integer
+    End Class
+
+    Public Class Iss_Position
+        Public Property Latitude As String
+        Public Property Longitude As String
+    End Class
+    Dim marker As GMarkerGoogle
+    Dim markerOverlay As GMapOverlay
+    Dim dt As DataTable
+    Dim LatInicial As Double = -31.5555087
+    Dim LongInicial As Double = -63.5364147
+    Dim ubica As New Rootobject
+
+
+    Dim serieCirculo1 As New Series("ISS_Position") With {
+        .ChartType = SeriesChartType.Spline}
+
+
+#End Region
+
+    Public Function Buscardatos(url As String) As String
+        Dim source As String = ""
+
+        Dim request As HttpWebRequest = CType(WebRequest.Create(url), HttpWebRequest)
+        Dim response As HttpWebResponse = CType(request.GetResponse(), HttpWebResponse)
+
+        If response.StatusCode = HttpStatusCode.OK Then
+            Dim receiveStream As Stream = response.GetResponseStream()
+            Dim readStream As StreamReader = Nothing
+
+            readStream = New StreamReader(receiveStream)
+
+            source = readStream.ReadToEnd()
+
+            response.Close()
+            readStream.Close()
+        Else
+            source = response.StatusDescription
+        End If
+        Return source
+    End Function
+    Public Sub DeserealizarJSON()
+
+        Dim json As String = txtCodigo.Text
+        Newtonsoft.Json.JsonConvert.PopulateObject(json, ubica)
+
+        ListBox1.Items.Add("Latitud: " + Convert.ToString(ubica.Iss_position.Latitude))
+        ListBox1.Items.Add("Longitud: " + Convert.ToString(ubica.Iss_position.Longitude))
+        ListBox1.Items.Add("Hora: " + CStr(TimeOfDay))
+        ListBox1.Items.Add("***************")
+    End Sub
+    Public Sub MarcarRuta()
+        Dim ruta As GMapOverlay = New GMapOverlay("CapaRuta")
+        Dim puntos As List(Of PointLatLng) = New List(Of PointLatLng)
+        'markerOverlay = New GMapOverlay("Marcador")
+        markerOverlay = GMapControl1.Overlays.First(Function(d) d.Id = "Marcador")
+
+        marker = CType(markerOverlay.Markers.First, GMarkerGoogle)
+        marker.Position = New PointLatLng(serieCirculo1.Points.Last.YValues(0), serieCirculo1.Points.Last.XValue)
+
+        'markerOverlay.Markers.Add(marker)
+        marker.ToolTipMode = MarkerTooltipMode.Always
+        marker.ToolTipText = String.Format("Ubicacion: Latitud:{0} Longitud: {1}",
+                                           serieCirculo1.Points.Last.YValues(0), serieCirculo1.Points.Last.XValue)
+        GMapControl1.Overlays.Add(markerOverlay)
+
+
+
+        For Each dato In serieCirculo1.Points
+            puntos.Add(New PointLatLng(dato.YValues(0), dato.XValue))
+        Next
+
+        Dim PuntosRuta As GMapRoute = New GMapRoute(puntos, "Ruta")
+
+        ruta.Routes.Add(PuntosRuta)
+        GMapControl1.Overlays.Add(ruta)
+        GMapControl1.Refresh()
+        GMapControl1.ZoomAndCenterMarkers("Marcador")
+        GMapControl1.Zoom = 3
+    End Sub
+    Public Sub DatosMapaenChart()
+        Chart1.Series.Clear()
+
+        serieCirculo1.Points.AddXY(Convert.ToDouble(ubica.Iss_position.Longitude.Replace(".", ",")),
+                                 Convert.ToDouble(ubica.Iss_position.Latitude.Replace(".", ",")))
+
+        Chart1.Series.Add(serieCirculo1)
+        Chart1.ChartAreas(0).AxisY.Minimum = -90
+        Chart1.ChartAreas(0).AxisY.Maximum = 90
+        Chart1.ChartAreas(0).AxisX.Minimum = -180
+        Chart1.ChartAreas(0).AxisX.Maximum = 180
+
+    End Sub
+    Public Sub CargarGoogleMap()
+        GMapControl1.DragButton = MouseButtons.Left
+        GMapControl1.CanDragMap = True
+        GMapControl1.MapProvider = GMapProviders.GoogleMap
+        GMapControl1.Position = New PointLatLng(LatInicial, LongInicial)
+        GMapControl1.MinZoom = 0
+        GMapControl1.MaxZoom = 24
+        GMapControl1.Zoom = 2
+        GMapControl1.AutoScroll = True
+
+        markerOverlay = New GMapOverlay("Marcador")
+        marker = New GMarkerGoogle(New PointLatLng(LatInicial, LongInicial), GMarkerGoogleType.green_dot)
+        markerOverlay.Markers.Add(marker)
+        marker.ToolTipMode = MarkerTooltipMode.Always
+        marker.ToolTipText = String.Format("Ubicacion: Latitud:{0} Longitud: {1}", LatInicial, LongInicial)
+
+        GMapControl1.Overlays.Add(markerOverlay)
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        LblURLWiki.Text = "https://es.wikipedia.org/w/api.php?action=parse&page=Estaci%C3%B3n_Espacial_Internacional&section=0&prop=text&format=json"
+        LblURL.Text = "http://api.open-notify.org/iss-now.json"
+        WebBrowser1.AllowNavigation = False
+
+        ' {"message": "success", "iss_position": {"latitude": "-35.5159", "longitude": "-27.0538"}, "timestamp": 1535083239}
+
+
+        txtCodigo.Text = Buscardatos(LblURL.Text)
+
+        DeserealizarJSON()
+        DatosMapaenChart()
+        CargarGoogleMap()
+
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+
+        txtCodigo.Text = Buscardatos(LblURL.Text)
+
+        DeserealizarJSON()
+        DatosMapaenChart()
+        MarcarRuta()
+
+        Label2.Text = "Cantidad de puntos: " + serieCirculo1.Points.Count.ToString
+
+    End Sub
+
+
+    Private Sub WebBrowser1_NewWindow(sender As Object, e As CancelEventArgs) Handles WebBrowser1.NewWindow
+        e.Cancel = True
+
+
+
+    End Sub
+#Region "Practica"
+    'Public Sub DatosTelefoniaChart()
+    '    Chart1.Series.Clear()
+
+    '    Dim serieCirculo As New Series("Mercado de Telefonía Celular")
+    '    serieCirculo.ChartType = SeriesChartType.Radar
+
+    '    serieCirculo.Points.AddXY("Claro", 5350)
+    '    serieCirculo.Points.AddXY("Movistar", 3425)
+    '    serieCirculo.Points.AddXY("Personal", 2450)
+    '    serieCirculo.Points.AddXY("lalala", 535)
+    '    serieCirculo.Points.AddXY("Otros", 535)
+    '    serieCirculo.Label = "#AXISLABEL #PERCENT"
+    '    serieCirculo.LegendText = "#AXISLABEL"
+
+    '    Chart1.Series.Add(serieCirculo)
+    'End Sub
+
+
+    'Public Sub BuscarenelString()
+    '    TextBoxDATOS.Clear() 'LIMPIA ANTES DE BUSCAR DATOS DE NUEVO
+    '    Try
+    '        Dim MIARRAY As New ArrayList 'PARA PODER MANIPULAR LOS DATOS(ORDENAR, DESVIAR, ETC)
+    '        For Each ETIQUETA As HtmlElement In WebBrowser1.Document.All 'LEE TODO EL HTML ETIQUETA A ETIQUETA
+    '            If ETIQUETA.TagName = "td" And ETIQUETA.GetAttribute("id").Contains("DisplayWorkingSatLongitude") Then 'ETIQUETA ARTICULO
+    '                If ETIQUETA.OuterHtml.Contains("width=") Then
+    '                    MIARRAY.Add(ETIQUETA.InnerText) 'LA PONE EN EL ARRAY
+    '                End If
+    '            ElseIf ETIQUETA.TagName = "SPAN" And ETIQUETA.GetAttribute("id").Contains("price_index_page_") Then 'ETIQUETA PRECIO
+    '                MIARRAY.Add(ETIQUETA.InnerText) 'LA PONE EN EL ARRAY
+    '            ElseIf ETIQUETA.TagName = "SPAN" And ETIQUETA.GetAttribute("id").Contains("counter_index_page_") Then 'ETIQUETA TIEMPO
+    '                If ETIQUETA.OuterHtml.Contains("Finalizada") = False Then
+    '                    MIARRAY.Add(ETIQUETA.InnerText) 'LA PONE EN EL ARRAY
+    '                Else
+    '                    Exit For ' SI HA LLEGADO A UNA ETIQUETA DE FINALIZADA PARA EL BUCLE
+    '                End If
+    '            End If
+    '        Next
+    '        For I = 0 To MIARRAY.Count - 3 'PRESENTA LOS DATOS DE LAS SUBASTAS VIVAS EN EL TEXTBOX ELIMINANDO LOS DATOS QUE CORRESPONDEN A LA FINALIZADA 
+    '            If MIARRAY(I + 2).ToString.Contains("Finalizada") = False Then
+    '                TextBoxDATOS.Text += MIARRAY(I).ToString & vbCrLf
+    '            End If
+    '        Next
+    '    Catch ex As Exception
+    '        MsgBox(ex.Message)
+    '    End Try
+    'End Sub
+    'If array.Length <> Nothing Then
+
+    '    For Each dato In array
+    '        Dim posicion As Integer = dato.IndexOf("latitude")
+
+    '        ' MsgBox(dato.Substring(posicion, 8))
+
+    '    Next
+    'End If
+
+    ''Dim asd As String = txtCodigo.Text
+    ''Dim starIndex As Integer = asd.IndexOf("latitude" + "'" + ":")
+    ''Dim endIndex As Integer = asd.IndexOf("'" + "long")
+    ''latitude = asd.Substring(starIndex + 1, asd.Length - endIndex + 1)
+    ''Dim numero As String() = latitude.Split(":"c)
+    ''Dim lat As String = "-" + numero(0)
+    ''MsgBox(lat)
+
+
+    '    Int starIndex = asd.IndexOf("latitude" + '"' + ":");
+    '    Int() endIndex = asd.IndexOf('"' + "long");
+    'String nr2 = asd.Substring(starIndex, asd.Length - endIndex);
+    '    String[] numero = nr2.Split('-');
+    '    String link = "-" + numero[1];           
+    '    MessageBox.Show(link);
+
+    'Private Sub GMapControl1_DoubleClick(sender As Object, e As EventArgs) Handles GMapControl1.DoubleClick
+    '    Dim lat As Double = GMapControl1.FromLocalToLatLng(e.X, e.Y).Lat
+    '    Dim lng As Double = GMapControl1.FromLocalToLatLng(e.X, e.Y).Lng
+
+
+    '    'Creamos el marcador para moverlo al lugar indicado 
+    '    '  marker.Position = New PointLatLng(lat, lng)
+    '    'Tambien se agrega el mensaje al marcador (tooltip) 
+    '    ' marker.ToolTipText = String.Format("Ubicacion: \n Latitud: {0} \n Longitud: {1}", lat, lng)
+    'End Sub
+
+    'Private Sub GMapControl1_MouseDoubleClick(sender As Object, e As MouseEventArgs) Handles GMapControl1.MouseDoubleClick
+    '    Dim lat As Double = GMapControl1.FromLocalToLatLng(e.X, e.Y).Lat
+    '    Dim lng As Double = GMapControl1.FromLocalToLatLng(e.X, e.Y).Lng
+
+
+    '    ' Creamos el marcador para moverlo al lugar indicado 
+    '    marker.Position = New PointLatLng(lat, lng)
+    '    '  Tambien se agrega el mensaje al marcador (tooltip) 
+    '    marker.ToolTipText = String.Format("Ubicacion: \n Latitud: {0} \n Longitud: {1}", lat, lng)
+    'End Sub
+
+    'markerOverlay = New GMapOverlay("Marcador")
+    'marker = New GMarkerGoogle(New PointLatLng(LatInicial, LongInicial), GMarkerGoogleType.green_dot)
+    'markerOverlay.Markers.Add(marker)
+    'marker.ToolTipMode = MarkerTooltipMode.Always
+    'marker.ToolTipText = String.Format("Ubicacion: Latitud:{0} Longitud: {1}", LatInicial, LongInicial)
+
+    'GMapControl1.Overlays.Add(markerOverlay)
+
+#End Region
+End Class
